93f03d78ec07c0e55b63dfd5d0b46d916e91c8ed
Maic Miller <maicmiller42@gmail.com>
2021-03-31 16:49:40 -0300

    :construction: Data Fetch
    - Buscando os dados com fetch, e manipulando-os.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Oi Mundo.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    this.loadposts();
+  }
+
+  loadposts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    // Zipper: unir dois arrays.
+    const postAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postAndPhotos });
+
+  }
+
+  render() {
+    const { posts, photos } = this.state;
+
+    return (
+      <section className="container">
+
+        <div className="posts">
+
+          {posts.map(post => (
+
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content" >
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
-
 export default App;
