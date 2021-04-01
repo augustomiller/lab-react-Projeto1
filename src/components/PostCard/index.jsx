ba757166bb546849b44615627a3f49c1148bafb1
Maic Miller <maicmiller42@gmail.com>
2021-03-31 21:58:34 -0300

    :hammer: Organizando Comopnentes
    - Iciando uma organizacao basica na estrutura do projeto e das funcoes referente aos componentes.

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+export const PostCard = ({ title, cover, body, id }) => {
+
+    return (
+        <div className="post">
+            <img src={cover} alt={title} />
+            <div className="post-content" >
+                <h1>{title}</h1>
+                <p>{body}</p>
+            </div>
+        </div>
+    );
+}
