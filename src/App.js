5f09549ea64fadc311cfed331c204948c1c50f32
Maic Miller <maicmiller42@gmail.com>
2021-03-31 23:09:09 -0300

    :hammer: Organizando os componentes parte 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import './App.css';
 import { Component } from 'react';
-import { PostCard } from './components/PostCard';
+import './App.css';
+import { Posts } from './components/Posts';
+import { loadPosts } from './utils/load-posts';
 
 class App extends Component {
   state = {
     posts: []
   };
 
-  componentDidMount() {
-    this.loadposts();
+  async componentDidMount() {
+    await this.loadPosts();
   }
 
-  loadposts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
-
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    // Zipper: unir dois arrays.
-    const postAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url }
-    });
-
-    this.setState({ posts: postAndPhotos });
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
   }
 
   render() {
@@ -33,19 +22,7 @@ class App extends Component {
 
     return (
       <section className="container">
-
-        <div className="posts">
-
-          {posts.map(post => (
-            <PostCard
-              key={post.id}
-              title={post.title}
-              body={post.body}
-              id={post.id}
-              cover={post.cover}
-            />
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
     );
   }
--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,12 +1,9 @@
-export const PostCard = ({ title, cover, body, id }) => {
-
-    return (
-        <div className="post">
-            <img src={cover} alt={title} />
-            <div className="post-content" >
-                <h1>{title}</h1>
-                <p>{body}</p>
-            </div>
+export const PostCard = ({ title, cover, body, id }) => (
+    <div className="post">
+        <img src={cover} alt={title} />
+        <div className="post-content" >
+            <h2>{title}</h2>
+            <p>{body}</p>
         </div>
-    );
-}
+    </div>
+);
